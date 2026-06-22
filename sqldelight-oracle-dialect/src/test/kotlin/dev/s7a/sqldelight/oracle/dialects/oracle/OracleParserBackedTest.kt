b1c3ee3898b21decd28b3caeaf3ddf6d71c0a788
sya-ri <git@s7a.dev>
2026-06-23 02:42:24 +0900

    Add Oracle alter table clustering coverage

--- sqldelight-oracle-dialect/src/test/kotlin/dev/s7a/sqldelight/oracle/dialects/oracle/OracleParserBackedTest.kt
+++ sqldelight-oracle-dialect/src/test/kotlin/dev/s7a/sqldelight/oracle/dialects/oracle/OracleParserBackedTest.kt
@@ -3600,10 +3600,17 @@ class OracleParserBackedTest :
                 ALTER TABLE alter_advanced_targets ILM DELETE_ALL;
 
                 ALTER TABLE alter_advanced_targets
+                ADD CLUSTERING BY INTERLEAVED ORDER ((id, status), created_at)
+                  YES ON LOAD
+                  NO ON DATA MOVEMENT;
+
+                ALTER TABLE alter_advanced_targets
                 MODIFY CLUSTERING BY LINEAR ORDER (id, created_at)
                   YES ON LOAD
                   YES ON DATA MOVEMENT
                   WITH MATERIALIZED ZONEMAP;
+
+                ALTER TABLE alter_advanced_targets DROP CLUSTERING;
 
                 ALTER TABLE alter_advanced_targets
                 ADD PARTITION p_2026 VALUES LESS THAN (DATE '2027-01-01') TABLESPACE users;
